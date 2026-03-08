7583bf5c8746b749c11dc3fb538546e324d5dd82
my first github <sayeedomar359@gmail.com>
2026-03-08 12:17:35 +0600

    delete script.js because i can add on index.html by only script tag for login form

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("btn1").addEventListener("click", function() {
-    if(username.value === "admin" && password.value === "admin123") {
-        alert("Login successful!");
-        window.location.href = "dashboard.html";
-    } else {
-        alert("Login failed!");
-    }
-});
-
